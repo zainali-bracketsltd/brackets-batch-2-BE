5c036b4a74d8aa4d87ed79813874112be8c9471c
Zain Ali <zain@bracketsltd.email>
2022-10-28 10:15:30 +0500

    class 2022-10-28

--- 2022-10-26/express/server.js
+++ 2022-10-26/express/server.js
@@ -9,26 +9,58 @@ const app = express()
 // body parser
 app.use(express.json({ extended: false }))
 
+app.use((req, res, next) => {
+  // write logic
+  // change req or res objects
+  // send response or end response cycle
+  // pass to next middleware
+
+  const url = req.originalUrl
+  const method = req.method
+
+  console.log(`Request received method -> ${method}, url -> ${url} `)
+
+  req.instructor = 'Zain'
+
+  next()
+})
+
 /**
  * list all users
  */
 app.get('/users', (req, res) => {
   const users = listUsers()
 
-  res.send(users)
+  res.send({ instructor: req.instructor, users })
 })
 
 /**
  * get a user with id
  * @param userId
  */
-app.get('/users/:userId', (req, res) => {
-  const { userId } = req.params
+app.get(
+  '/users/:userId',
+  (req, res, next) => {
+    const { userId } = req.params
 
-  const user = getUser(userId)
+    isNaN(userId) ? res.send({ error: 'Invalid userId type.' }) : next()
 
-  res.send(user)
-})
+    // if (isNaN(userId)) {
+    //   res.send({
+    //     error: 'Invalid userId type.'
+    //   })
+    // } else {
+    //   next()
+    // }
+  },
+  (req, res) => {
+    const { userId } = req.params
+
+    const user = getUser(userId)
+
+    res.send(user)
+  }
+)
 
 /**
  * Add a user to DB
@@ -44,10 +76,34 @@ app.post('/users', (req, res) => {
 /**
  * update an existing user
  */
+app.patch('/users/:userId', (req, res) => {
+  try {
+    const user = req.body
+
+    let users = listUsers()
+
+    users = users.map(u => {
+      if (u.id === userId) {
+        return user
+      } else {
+        return user
+      }
+    })
+
+    fs.writeFileSync('users.json', JSON.stringify(users))
+
+    res.send('SUCCESS: user updated.')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 /**
  * delete a user
  */
+app.delete('/users/:userId', (req, res) => {
+  // delete user here
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
