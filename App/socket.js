362acb0b78a6c82f3595795b64f9062247bf3dcb
Zain Ali <zain@bracketsltd.email>
2022-12-09 09:15:37 +0500

    socket rooms

--- App/socket.js
+++ App/socket.js
@@ -4,12 +4,16 @@ function listenEvents (io) {
   io.on('connection', socket => {
     console.log('*** socket connected ***', socket.id)
 
+    socket.join('my-room')
+
     socket.emit('message', {
       text: 'Assalamu Alaikum!'
     })
 
     socket.on('message', message => {
       MessageService.createMessage(message)
+
+      socket.to('my-room').emit('message', message)
     })
   })
 }
